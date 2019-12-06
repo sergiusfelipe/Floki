62cc6eff646d3a353c3873d9c6a260de21acf3a7
Sergio Felipe <33283108+sergiusfelipe@users.noreply.github.com>
2019-12-06 09:29:11 -0300

    Update gwo.py

--- gwo.py
+++ gwo.py
@@ -4,13 +4,13 @@ import math
 from solution import solution
 import time
 import floki_3
-
+import csv
     
 
-def GWO(P, I, D, lb, ub, dim, SearchAgents_no):
+def GWO(P, I, D, lb, ub, dim, SearchAgents_no, ite):
     
     PID = [P, I, D]
-    floki = FLOKI.FLOKI(P, I, D)
+    floki = floki_3.FLOKI(P, I, D, samples=3)
 
     #Max_iter=1000
     #lb=-100
@@ -44,6 +44,9 @@ def GWO(P, I, D, lb, ub, dim, SearchAgents_no):
     
     Convergence_curve=[]
     s=solution()
+    kp_curve=numpy.zeros(ite)
+    ki_curve=numpy.zeros(ite)
+    kd_curve=numpy.zeros(ite)
 
      # Loop counter
     print("GWO is optimizing  Floki")    
@@ -51,7 +54,7 @@ def GWO(P, I, D, lb, ub, dim, SearchAgents_no):
     timerStart=time.time() 
     s.startTime=time.strftime("%Y-%m-%d-%H-%M-%S")
     # Main loop
-    while True:
+    for l in range(0,ite):
         for i in range(0,SearchAgents_no):
             
             # Return back the search agents that go beyond the boundaries of the search space
@@ -59,12 +62,15 @@ def GWO(P, I, D, lb, ub, dim, SearchAgents_no):
                 Positions[i,j]=numpy.clip(Positions[i,j], lb[j], ub[j])
 
             # Calculate objective function for each search agent
-            fitness= floki.controle(Positions[i,:])
+            fitness = floki.controle(pos[i,0],pos[i,1],pos[i,2])
             
             # Update Alpha, Beta, and Delta
             if fitness<Alpha_score :
                 Alpha_score=fitness; # Update alpha
                 Alpha_pos=Positions[i,:].copy()
+                kp_curve[l]=Positions[i,0]
+                ki_curve[l]=positions[i,1]
+                kd_curve[l]=Positions[i,2]
             
             
             if (fitness>Alpha_score and fitness<Beta_score ):
@@ -79,7 +85,7 @@ def GWO(P, I, D, lb, ub, dim, SearchAgents_no):
         
         
         
-        a=2-l*((2)/Max_iter); # a decreases linearly fron 2 to 0
+        a=2-l*((2)/ite); # a decreases linearly fron 2 to 0
         
         # Update the Position of search agents including omegas
         for i in range(0,SearchAgents_no):
@@ -125,8 +131,37 @@ def GWO(P, I, D, lb, ub, dim, SearchAgents_no):
     timerEnd=time.time()  
     s.endTime=time.strftime("%Y-%m-%d-%H-%M-%S")
     s.executionTime=timerEnd-timerStart
-    s.convergence=Convergence_curve
-    s.optimizer="GWO"
+    s.convergence=convergence_curve
+    s.optimizer="PSO"
     s.objfname="Floki"
+    s.kp_convergence = kp_curve
+    s.ki_convergence = ki_curve
+    s.kd_convergence = kd_curve
+    s.kp = kp_curve[-1]
+    s.ki = ki_curve[-1]
+    s.kd = kd_curve[-1]
     
-gwo = GWO(100, 10, 5, -0.5, 0.5, 3, 6)
+    return s
+
+
+P = 20
+I = 4.5
+D = 0.001
+iters = 50
+gwo = GWO(P, I, D, -1.5, 1.5, 3, 50, iters)
+print("Otimizacao feita")
+Flag = False
+Export=True
+if(Export==True):
+    with open(ExportToFile, 'a',newline='\n') as out:
+        writer = csv.writer(out,delimiter=' ')
+        if (Flag==False): # just one time to write the header of the CSV file
+            for i in range(0,iters):
+                header= numpy.concatenate([i,pso.kp_convergence[i], pso.ki_convergence[i], pso.kd_convergence[i], pso.convergence[i]])
+                writer.writerow(header)
+    out.close()
+    Flag = True
+
+if Flag == True:
+    while True:
+        floki.controle(pso.kp, pso.ki, pso.kd)
