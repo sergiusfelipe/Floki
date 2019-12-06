ad9df918a78d813fd25e5f940f4e7733fa195842
Sergio Felipe <33283108+sergiusfelipe@users.noreply.github.com>
2019-12-06 15:58:51 -0300

    Create food_source.py

---
+++ food_source.py
@@ -0,0 +1,12 @@
+class FoodSource(object):
+    trials = 0
+
+    """docstring for FoodSource"""
+    def __init__(self, initial_solution, initial_fitness):
+        super(FoodSource, self).__init__()
+
+        self.solution = initial_solution
+        self.fitness = initial_fitness
+
+    def __repr__(self):
+        return f'<FoodSource s:{self.solution} f:{self.fitness} />' 
